c37e21987ec1d8d7b6c64ca723d7c8873818c907
PhilVoel <philipp123.voeller@gmail.com>
2024-11-27 19:09:31 +0100

    Init

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+	description = "Bachelorprojekt";
+
+	inputs = {
+		nixpkgs.url = "nixpkgs/nixos-unstable";
+		flake-utils.url = "github:numtide/flake-utils/v1.0.0";
+	};
+
+	outputs = {flake-utils, nixpkgs, ...}:
+		flake-utils.lib.eachDefaultSystem (system:
+			let pkgs = nixpkgs.legacyPackages."${system}";
+			in {
+				devShells.default = pkgs.mkShell {
+					name = "Bachelorprojekt shell";
+					packages = with pkgs; [
+						jdk
+					];
+				};
+			}
+		);
+}
