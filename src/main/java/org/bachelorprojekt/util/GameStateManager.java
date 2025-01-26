b23be0ed5b097350911b4913a27ff8983ba0700d
David Hermann <redeagle.private@gmail.com>
2025-01-26 18:42:48 +0100

    Adding getPlayer method to GameStateManager Adding inventory methods and removing Quest methods from GameStateManager

--- src/main/java/org/bachelorprojekt/util/GameStateManager.java
+++ src/main/java/org/bachelorprojekt/util/GameStateManager.java
@@ -2,43 +2,32 @@ package org.bachelorprojekt.util;
 
 import org.bachelorprojekt.game.Chapter;
 import org.bachelorprojekt.character.Player;
-import org.bachelorprojekt.game.Story;
 
 import java.util.List;
 
 public class GameStateManager {
     private List<Chapter> chapters; // Story aus JSON geladen
     private Player player;          // Spielerreferenz
+    private boolean isInventoryOpen = false;
 
     public GameStateManager(List<Chapter> chapters, Player player) {
         this.chapters = chapters;
         this.player = player;
     }
 
+    public Player getPlayer() {
+        return player;
+    }
+
     public Chapter getCurrentChapter() {
         return chapters.get(player.getCurrentChapterIndex());
     }
 
-    public Story.Quest getCurrentQuest() {
-        Chapter currentChapter = getCurrentChapter();
-        return currentChapter.getQuests().get(player.getCurrentQuestIndex());
+    public boolean isInventoryOpen() {
+        return isInventoryOpen;
     }
 
-    public void completeQuest() {
-        Story.Quest currentQuest = getCurrentQuest();
-        currentQuest.complete();
-
-        // Prüfen, ob es weitere Quests im Kapitel gibt
-        Chapter currentChapter = getCurrentChapter();
-        if (currentChapter.getQuests().size() > player.getCurrentQuestIndex() + 1) {
-            player.setCurrentQuestIndex(player.getCurrentQuestIndex() + 1);
-        } else {
-            // Kapitel abschließen und zum nächsten wechseln
-            currentChapter.setCompleted(true);
-            if (chapters.size() > player.getCurrentChapterIndex() + 1) {
-                player.setCurrentChapterIndex(player.getCurrentChapterIndex() + 1);
-                player.setCurrentQuestIndex(0); // Nächste Kapitel startet bei der ersten Quest
-            }
-        }
+    public void setInventoryOpen(boolean inventoryOpen) {
+        isInventoryOpen = inventoryOpen;
     }
 }
