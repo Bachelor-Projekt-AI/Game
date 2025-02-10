9e58fcab714f3f1ab4d5008168bb6bb2d16ac973
Friederike von Gruben <friederikevongruben@gmail.com>
2025-02-10 18:10:30 +0100

    Adding new fields to items
    - rarity
    - sell_price
    - buy_price
    - category

--- src/main/java/org/bachelorprojekt/util/json/jackson/Item.java
+++ src/main/java/org/bachelorprojekt/util/json/jackson/Item.java
@@ -1,6 +1,7 @@
 package org.bachelorprojekt.util.json.jackson;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Item {
@@ -9,6 +10,22 @@ public class Item {
     private String description;
     private boolean stackable;
     private int maxStackSize;
+    private String rarity;
+
+    @JsonProperty("sell_price")
+    private int sellPrice;
+
+    @JsonProperty("buy_price")
+    private int buyPrice;
+
+    private String category;
+    /*
+    * "rarity": "rare",
+      "sell_price": 100,
+      "buy_price": 1000,
+      "category": "weapon"
+    *
+    * */
 
     public Item() {
         this.id = 0;
@@ -16,6 +33,10 @@ public class Item {
         this.description = "";
         this.stackable = false;
         this.maxStackSize = 0;
+        this.rarity = "";
+        this.sellPrice = 0;
+        this.buyPrice = 0;
+        this.category = "";
     }
 
     // Getters
@@ -37,5 +58,21 @@ public class Item {
 
     public int getMaxStackSize() {
         return maxStackSize;
+    }
+
+    public String getRarity() {
+        return rarity;
+    }
+
+    public int getSellPrice() {
+        return sellPrice;
+    }
+
+    public int getBuyPrice() {
+        return buyPrice;
+    }
+
+    public String getCategory() {
+        return category;
     }
 }
