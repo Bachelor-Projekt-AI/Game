9a1af8ab5ed31b51d7f8ea2f2987a95188c8c00d
jannisfingerhut <jannis.fingerhut@informatik.hs-fulda.de>
2025-02-11 15:22:38 +0100

    Update JavaDoc #9

--- src/main/java/org/bachelorprojekt/game/events/LocationReachEvent.java
+++ src/main/java/org/bachelorprojekt/game/events/LocationReachEvent.java
@@ -2,14 +2,27 @@ package org.bachelorprojekt.game.events;
 
 import org.bachelorprojekt.game.GameEvent;
 
+/**
+ * Repräsentiert ein Spielereignis, das ausgelöst wird, wenn ein Spieler eine bestimmte Location erreicht.
+ */
 public class LocationReachEvent implements GameEvent {
 
     private final int locationId;
 
+    /**
+     * Erstellt ein neues Location-Erreichen-Ereignis.
+     *
+     * @param locationId Die ID der erreichten Location.
+     */
     public LocationReachEvent(int locationId) {
         this.locationId = locationId;
     }
 
+    /**
+     * Gibt die ID der erreichten Location zurück.
+     *
+     * @return Die ID der Location.
+     */
     public int getLocationId() {
         return locationId;
     }
--- src/main/java/org/bachelorprojekt/game/events/ObjectExamineEvent.java
+++ src/main/java/org/bachelorprojekt/game/events/ObjectExamineEvent.java
@@ -2,13 +2,30 @@ package org.bachelorprojekt.game.events;
 
 import org.bachelorprojekt.game.GameEvent;
 
+/**
+ * Ein Ereignis, das eine Untersuchung eines Objekts im Spiel darstellt.
+ * Dieses Ereignis enthält den Namen (oder die ID) des Objekts, das untersucht wird.
+ * 
+ * @author [Dein Name]
+ * @version 1.0
+ */
 public class ObjectExamineEvent implements GameEvent {
     private final String objectName; // oder Object-ID
 
+    /**
+     * Konstruktor, der ein Ereignis für die Untersuchung eines Objekts mit dem angegebenen Objektnamen erstellt.
+     * 
+     * @param objectName Der Name des Objekts, das untersucht wird.
+     */
     public ObjectExamineEvent(String objectName) {
         this.objectName = objectName;
     }
 
+    /**
+     * Gibt den Namen des Objekts zurück, das untersucht wird.
+     * 
+     * @return Der Name des Objekts.
+     */
     public String getObjectName() {
         return objectName;
     }
